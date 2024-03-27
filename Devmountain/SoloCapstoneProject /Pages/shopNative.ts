46ad506ee1d17ba91f74325000b420e35e5aa9ba
Aerosmitten <139496075+Aerosmitten@users.noreply.github.com>
2024-03-27 14:29:22 -0600

    Finished Featured Articles Test

--- Devmountain/SoloCapstoneProject /Pages/shopNative.ts
+++ Devmountain/SoloCapstoneProject /Pages/shopNative.ts
@@ -3,25 +3,27 @@ import { BasePage } from "./basePage"
 
 export class sn extends BasePage {
     //locators go here
-    bigSearchBtn: By = By. 
-    searchInput: By = By. 
-    searchListingBtn: By = By. 
-    clothingAndAcc: By = By. 
-    art: By = By. 
-    jewlery: By = By. 
-    food: By = By. 
-    beauty: By = By. 
-    beadwork: By = By. 
-    supplies: By = By. 
-    books: By = By. 
-    homeBtn: By = By. 
-    hoverShop: By = By. 
-    shopNative: By = By. 
-    viewAllListings: By = By. 
-    shopDrop: By = By. 
-    powWow: By = By. 
-    directoryOf: By = By. 
-    classifieds: By = By. 
+    bigSearchBtn: By = By.xpath('(//a[text()="Shop"])[3]');
+    searchInput: By = By.xpath('(//input[@name="q"])[2]');
+    searchListingBtn: By = By.xpath('//div[@class="directorist-search-form-action__submit"]');
+    results: By = By.xpath('//h1[text()="Search Result"]');
+    clothingAndAcc: By = By.xpath('//h4[text()="Clothing and Accessories"]');
+    art: By = By.xpath('//h4[text()="Art"]');
+    jewlery: By = By.xpath('//h4[text()="Jewelry"]');
+    food: By = By.xpath('//h4[text()="Food"]');
+    beauty: By = By.xpath('//h4[text()="Beauty"]');
+    beadwork: By = By.xpath('//h4[text()="Beadwork"]');
+    supplies: By = By.xpath('//h4[text()="Pow Wow Supplies & Regalia"]');
+    books: By = By.xpath('//h4[text()="Books"]');
+    homeBtn: By = By.xpath('//img[@class="logo ms-md-5 no-lazy"]');
+    hoverShop: By = By.xpath('(//a[text()="Shop"][1])');
+    shopNative: By = By.xpath('(//a[text()="Shop Native"])[1]');
+    viewAllListings: By = By.xpath('//button[text()="View all Listings"]');
+    shopDrop: By = By.xpath('(//a[text()="Shop"])[1]');
+    powWow: By = By.xpath('(//a[text()="Powwows.com Stores"])[1]');
+    shopNow: By = By.xpath('(//button[text()="Shop Now"])[1]');
+    directoryOf: By = By.xpath('(//button[text()="Shop Now"])[2]');
+    classifieds: By = By.xpath('(//a[text()="Classifieds"])[1]');
 
     constructor () {
         super ({url: "https://www.powwows.com/"});
@@ -32,5 +34,11 @@ export class sn extends BasePage {
         .move({origin: scrollThing}) 
         .perform(); 
         await this.driver.sleep(2000)
+    };
+    async canHover(elementBy:By) {
+        const hover = this.driver.actions();
+        const startElement = await this.getElement(elementBy);
+        await this.actionWiggle(hover, startElement, 100);
+        await hover.perform();
     };
 };
--- Devmountain/SoloCapstoneProject /Tests/shopNative.test.ts
+++ Devmountain/SoloCapstoneProject /Tests/shopNative.test.ts
@@ -10,104 +10,173 @@ describe ("Testing PowWows.com for Solo Capstone Project.", () => {
     afterAll (async () => {
         await page.driver.quit();
     });
-test ("Testing Shopping from the Big Search Button and using the search input", async () => {
+test.skip ("Testing Shopping from the Big Search Button and using the search input", async () => {
     //Scroll down to the Big Search Buttons
-
+    await page.scroll(page.bigSearchBtn);
     //Click "Shop Native"
-
+    await page.click(page.bigSearchBtn);
     //Click into input field
-
+    await page.click(page.searchInput);
     //Enter "Ribbin Skirt"
-
+    await page.sendKeys(page.searchInput, "Ribbon Skirt");
     //Click "Search Listing"
-
+    await page.click(page.searchListingBtn);//can't quite get to the results, possibly wrong locator for submit button??
+    await page.driver.sleep(2000);
     //Screenshot results
-
+    fs.writeFile(`${__dirname}/shop-bsb-input.png`,
+    await page.driver.takeScreenshot(), 'base64',
+    async (e) => {
+        if (e) console.error(e)
+        else console.log("Screenshot Ribbon Skirt Successful")
+    });
 });
-test ("Testing Shopping from the Big Search Button and trying all of the Popular Categories", async () => {
+test.skip ("Testing Shopping from the Big Search Button and trying all of the Popular Categories", async () => {//Not getting the correct screenshots
     //Scroll down to the Big Search Buttons
-
+    await page.scroll(page.bigSearchBtn);
     //Click "Shop Native"
-
+    await page.click(page.bigSearchBtn);
     //Click "Clothings and Accessories"
-
+    await page.click(page.clothingAndAcc)
     //Screenshot results
-
+    fs.writeFile(`${__dirname}/shop-bsb-clothing-accessories.png`,
+    await page.driver.takeScreenshot(), 'base64',
+    async (e) => {
+        if (e) console.error(e)
+        else console.log("Screenshot Clothing and Accessories Successful")
+    });
     //Click Home button
-
+    await page.click(page.homeBtn);
     //Click "art"
-
+    await page.click(page.art);
     //Screenshot resluts
-
+    fs.writeFile(`${__dirname}/shop-bsb-Attr.png`,
+    await page.driver.takeScreenshot(), 'base64',
+    async (e) => {
+        if (e) console.error(e)
+        else console.log("Screenshot Art Successful")
+    });
     //Click Home button
-
+    await page.click(page.homeBtn);
     //Click "Jewlery"
-
+    await page.click(page.jewlery);
     //Screenshot results
-
+    fs.writeFile(`${__dirname}/shop-bsb-jewlery.png`,
+    await page.driver.takeScreenshot(), 'base64',
+    async (e) => {
+        if (e) console.error(e)
+        else console.log("Screenshot Jewlery Successful")
+    });
     //Click Home button
-
+    await page.click(page.homeBtn);
     //Click "Food"
-
+    await page.click(page.food);
     //Screenshot results
-
+    fs.writeFile(`${__dirname}/shop-bsb-food.png`,
+    await page.driver.takeScreenshot(), 'base64',
+    async(e) => {
+        if (e) console.error(e)
+        else console.log("Screenshot Food Succesful")
+    });
     //Click Home button
-
+    await page.click(page.homeBtn);
     //Click "Beauty"
-
+    await page.click(page.beauty);
     //Screenshot results
-
+    fs.writeFile(`${__dirname}/shop-bsb-beauty.png`,
+    await page.driver.takeScreenshot(), 'base64',
+    async(e) => {
+        if (e) console.error(e)
+        else console.log("Screenshot Beauty Successful")
+    });
     //Click Home button
-
+    await page.click(page.homeBtn);
     //Click "Beadwork"
-
+    await page.click(page.beadwork);
     //Screenshot results
-
+    fs.writeFile(`${__dirname}/shop-bsb-beadwork.png`,
+    await page.driver.takeScreenshot(), 'base64',
+    async(e) => {
+        if (e) console.error(e)
+        else console.log("Screenshot Beadwork Successful")
+    });
     //Click Home button
-
+    await page.click(page.homeBtn);
     //Click "Pow Wow Supplies & Regalia"
-    
+    await page.click(page.supplies);
     //Screenshot results
-
+    fs.writeFile(`${__dirname}/shop-bsb-supples.png`,
+    await page.driver.takeScreenshot(), 'base64',
+    async(e) => {
+        if (e) console.error(e)
+        else console.log("Screenshot Supplies Successful")
+    });
     //Click Home button
-
+    await page.click(page.homeBtn);
     //Click "Books"
-
+    await page.click(page.books);
     //Screenshot results
-
+    fs.writeFile(`${__dirname}/shop-bsb-books.png`,
+    await page.driver.takeScreenshot(), 'base64',
+    async (e) => {
+        if (e) console.error(e)
+        else console.log("Screenshot Books Successful")
+    });
 
 });
-test ("Testing Shopping from the Dropdown Menu and View all Listings", async () => {
+test.skip ("Testing Shopping from the Dropdown Menu and View all Listings", async () => { //Not getting screenshot of the correct thing
     //Hover over "Shop" Dropdown Menu
-
+    await page.canHover(page.hoverShop);
     //Click "Shop Native"
-
+    await page.click(page.shopNative);
     //Click "View all Listings"
-
+    await page.click(page.viewAllListings);
     //Screenshot results
-
+    fs.writeFile(`${__dirname}/shop-drop-viewAll.png`,
+    await page.driver.takeScreenshot(), 'base64',
+    async (e) => {
+        if (e) console.error(e)
+        else console.log("Screenshot View All Successful")
+    });
 });
-test ("Testing Shopping by clicking the 'Shop' Dropdown Menu and Shop Pow Wows Merch", async () => {
+test.skip ("Testing Shopping by clicking the 'Shop' Dropdown Menu and Shop Pow Wows Merch", async () => { //Not getting screenshot of correct thing
     //Click the "Shop" dropdown Menu
-
+    await page.click(page.shopDrop);
     //Click "Shop Now" on Shop Pow Wows Merch
-
+    await page.click(page.shopNow);
     //Screenshot results
-
+    fs.writeFile(`${__dirname}/shop-drop-PowWow-Merch.png`,
+    await page.driver.takeScreenshot(), 'base64',
+    async (e) => {
+        if (e) console.error(e)
+        else console.log("Screenshot Pow Wow Merch Successful")
+    });
 });
-test ("Testing Shopping by clicking the 'Shop' Dropdown Menu and Directory of Native Products/Stores", async () => {
+test.skip ("Testing Shopping by clicking the 'Shop' Dropdown Menu and Directory of Native Products/Stores", async () => {//Not getting correct screenshot
     //Click the "Shop" Dropdown Menu
-
+    await page.click(page.shopDrop);
     //Click "Shop Now" on Directory of Native Products/Stores
-
+    await page.click(page.directoryOf);
     //Screenshot results
+    fs.writeFile(`${__dirname}/shop-drop-directory.png`,
+    await page.driver.takeScreenshot(), 'base64',
+    async (e) => {
+        if (e) console.error(e)
+        else console.log("Screenshot Directory of Native Products/Stores Successful")
+    });
 });
-test ("Testing Shopping from the Dropdown Menu and Pow Wows Classifieds", async () => {
+test ("Testing Shopping from the Dropdown Menu and Pow Wows Classifieds", async () => {//wrongs locator??
     //Click the "Shop" Dropdown Menu
-
-    //Click "Join Today" on Pow Wows Classifieds
-
+    await page.click(page.shopDrop);
+    await page.scroll(page.classifieds);    //Click "Join Today" on Pow Wows Classifieds
+  
+    await page.click(page.classifieds);
     //Screenshot results
+    fs.writeFile(`${__dirname}/shop-drop-classifieds.png`,
+    await page.driver.takeScreenshot(), 'base64',
+    async (e) => {
+        if (e) console.error(e)
+        else console.log("Screenshot Classifieds Successful")
+    });
 
 });
 });
